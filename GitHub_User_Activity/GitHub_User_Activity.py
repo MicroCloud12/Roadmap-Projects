bff26b5396903536dae07881bb7b85446d849307
MicroCloud12 <35950276+MicroCloud12@users.noreply.github.com>
2025-04-28 13:31:51 -0600

    a

--- GitHub_User_Activity/GitHub_User_Activity.py
+++ GitHub_User_Activity/GitHub_User_Activity.py
@@ -6,14 +6,12 @@ import urllib.request as request
 if len(sys.argv) < 2:
     print("Introduce un Usuario valido")
     sys.exit(1)
-
-    
 username = sys.argv[1]
 print(f"Obteniendo actividad para el usuario: {username}") # Mensaje de prueba
 
+
 URL = f"https://api.github.com/users/{username}/events"
 print(f"URL de la API a consultar: {URL}") # Mensaje de prueba
-
 req = request.Request(URL)
 response = request.urlopen(req)
 status_code = response.getcode()
@@ -33,9 +31,9 @@ if status_code == 200:
             print(f"- Pushed {numero_commits} commits to {nombre_repo}")
         elif data['type'] == 'IssuesEvent':
             nombre_repo = data['repo']['name']
-            lista_de_commits = data['payload']['commits']
-            numero_commits = len(lista_de_commits)
-            print(f"- Pushed {numero_commits} commits to {nombre_repo}")
+            action = data['payload']['action']
+            if action == 'opened':
+                print(f"- Opened a new issue in {nombre_repo}")
 else:
     print(f"No se conecto correctamente, código de error: {status_code}")
     sys.exit(1)
